371ae5062c4b833f04e25469c7f99376b81d1716
hilmyha <hilmyahmadhaidar@gmail.com>
2023-06-30 19:32:23 +0700

    push crud firebase

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.firebaseapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.firebaseapp.Fitur.Edit
+import com.example.firebaseapp.Fitur.Insert
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -21,13 +25,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val insertButton: Button = findViewById(R.id.insertButton)
+        insertButton.setOnClickListener {
+            insertMahasiswa()
+        }
+
         // Inisialisasi RecyclerView
         userRecyclerView = findViewById(R.id.recyclerView)
         userRecyclerView.layoutManager = LinearLayoutManager(this)
 
         // Inisialisasi ArrayList dan Adapter
         userList = ArrayList()
-        adapter = MahasiswaAdapter(userList)
+        adapter = MahasiswaAdapter(userList, this, ::editMahasiswa, ::deleteMahasiswa)
         userRecyclerView.adapter = adapter
 
         // Mendapatkan referensi database Firebase
@@ -55,5 +64,28 @@ class MainActivity : AppCompatActivity() {
                 // Penanganan kesalahan saat mengakses Firebase
             }
         })
+    }
+
+    private fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        // Delete the mahasiswa data from Firebase
+        val userId = mahasiswa.nim
+        userId?.let {
+            database.child(it).removeValue()
+        }
+    }
+
+    private fun editMahasiswa(mahasiswa: Mahasiswa) {
+        // Open the edit activity or perform the edit operation as needed
+        val intent = Intent(this, Edit::class.java)
+        intent.putExtra("nama", mahasiswa.nama)
+        intent.putExtra("nim", mahasiswa.nim)
+        intent.putExtra("telp", mahasiswa.telp)
+        startActivity(intent)
+
+    }
+
+    private fun insertMahasiswa() {
+        val intent = Intent(this, Insert::class.java)
+        startActivity(intent)
     }
 }
